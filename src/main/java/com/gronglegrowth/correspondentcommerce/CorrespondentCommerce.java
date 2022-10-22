32ce9fabc9e9f6a92afec7021983c6996be7b0db
Llull <32804557+FyreAlchemage@users.noreply.github.com>
2022-10-22 19:24:56 -0400

    Add rubedo shards to coal loot-table for testing

--- src/main/java/com/gronglegrowth/correspondentcommerce/CorrespondentCommerce.java
+++ src/main/java/com/gronglegrowth/correspondentcommerce/CorrespondentCommerce.java
@@ -2,9 +2,13 @@ package com.gronglegrowth.correspondentcommerce;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -20,6 +24,9 @@ public class CorrespondentCommerce implements ModInitializer {
 	public static final Item SHARD = new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON));
 	public static final ExchangeStoneItem EXCHANGE_STONE = new ExchangeStoneItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE));
 
+	private static final Identifier MAGMA_CUBE_LOOT = Blocks.COAL_ORE.getLootTableId();
+	// coal is for testing
+
 	@Override
 	public void onInitialize() {
 
@@ -27,6 +34,16 @@ public class CorrespondentCommerce implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("correspondentcommerce", "rubedo_shard"), SHARD);
 		Registry.register(Registry.ITEM, new Identifier("correspondentcommerce", "rubedo_stone"), EXCHANGE_STONE);
 		FuelRegistry.INSTANCE.add(SHARD, 12800);
+
+		// Inject rubedo shards into the magma cube's loot table.
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && MAGMA_CUBE_LOOT.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(SHARD));
+
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 
 		LOGGER.info("Correspondent Commerce has loaded!");
 	}
